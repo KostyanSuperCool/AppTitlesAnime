7042f56c3e73371746ef1faccddf15d237f1e96e
Киселев Константин <kiselev.k0101@gmail.com>
2025-04-03 14:10:42 +0500

    Установлены пакеты, добавлено подключение к базе данных, изменены имена навигационных свойст

---
+++ AppTitlesAnime/Models/Type.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppTitlesAnime.Models;
+
+public partial class Type
+{
+    public short Id { get; set; }
+
+    public string TypeName { get; set; } = null!;
+
+    //навигационные свойства
+    public virtual ICollection<AnimeTitile> AnimeTitiles { get; set; } = new List<AnimeTitile>();
+}
