22ed7f59089584408e6ebfce90e05f308aa4cc82
Киселев Константин <kiselev.k0101@gmail.com>
2025-04-10 14:59:33 +0500

    Добавлено удаление строки из таблицы Типы аниме

--- AppTitlesAnime/FormListTypes.Designer.cs
+++ AppTitlesAnime/FormListTypes.Designer.cs
@@ -87,6 +87,7 @@
             btnDeleteType.TabIndex = 2;
             btnDeleteType.Text = "Удалить";
             btnDeleteType.UseVisualStyleBackColor = true;
+            btnDeleteType.Click += BtnDeleteType_Click;
             // 
             // panelFill
             // 
